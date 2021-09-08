c11eff5f5a9e9c2ecac5f4693bbfcc01f0557072
Ravi <raviranjanojha777@gmail.com>
2021-09-08 22:48:22 +0530

    fetch data from api

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
-function validatePassword() {
-  const password = document.getElementById('pwd');
-  const confirmPassword = document.getElementById('cnf-pwd');
-  let errors = [];
-  if (password.value !== confirmPassword.value)
-    errors.push('Passwords do not match!');
-  if (password.value.length < 6)
-    errors.push('Password must have atleast 6 characters');
-  if (password.value.search(/[a-z]/) < 0)
-    errors.push('Password must have atleast one letter');
-  if (password.value.search(/[0-9]/) < 0)
-    errors.push('Password must have atleast one number');
+const api_url = 'https://www.acefrontend.com/c/critter/feed.json';
 
-  if (errors.length > 0) {
-    alert(errors.join('\n'));
-    return false;
-  }
-  return true;
+async function getapi(url) {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+
+  show(data);
 }
 
-document.querySelector('#signup').addEventListener('click', validatePassword);
+getapi(api_url);
+
+function renderDate(dateString) {
+  const date = new Date(dateString);
+  const locale = 'en-us';
+  const month = date.toLocaleString(locale, { month: 'short' });
+  const day = date.getDate();
+  return `${month} ${day}`;
+}
+
+function show(data) {
+  let cards = '';
+  for (let i = 0; i < data.feed.length; i++) {
+    cards += `<div class='card'>
+    <img class='avatar'
+      src=${data.feed[i].user.avatar}
+      />
+      <div class='creet'>
+      <div class='head'>
+      <p class='username'>@${data.feed[i].user.username}</p>
+      <p class='date'>${renderDate(data.feed[i].created_at)}</p>
+      </div>
+      <p class='content'>${data.feed[i].text}</p>
+      <p class='like'>Likes: ${data.feed[i].likes}</p>
+      </div>
+    </div>`;
+  }
+
+  document.getElementById('creets').innerHTML = cards;
+}
